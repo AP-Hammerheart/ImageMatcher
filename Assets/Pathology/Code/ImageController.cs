555837529fe4ef183a710064a60db7278cc72e13
Adrian <adrian.parneus@gmail.com>
2019-10-28 17:16:50 +0100

    pan&zoom image

--- Assets/Pathology/Code/ImageController.cs
+++ Assets/Pathology/Code/ImageController.cs
@@ -9,40 +9,49 @@ public class ImageController : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     private Vector3 distance = Vector3.zero;
     private float dist = 0f;
     private Vector3 offset = Vector3.zero;
+    private bool panZoomImage = true;
+    public Toggle panZoomToggle;
+    public Toggle moveSelectionToggle;
 
     // Start is called before the first frame update
-    void Awake() {
-
+    private void Start() {
+        panZoomImage = panZoomToggle.isOn;
     }
 
-    float x=1f, y = 1f;
+    float x = 1f, y = 1f;
 
     private void Update() {
-        x += transform.localScale.x * Input.GetAxis( "Mouse ScrollWheel" );
-        y += transform.localScale.y * Input.GetAxis( "Mouse ScrollWheel" );
-        transform.localScale = new Vector3( x, y, 1 );
+        if( panZoomImage ) {
+            x += transform.localScale.x * Input.GetAxis( "Mouse ScrollWheel" );
+            y += transform.localScale.y * Input.GetAxis( "Mouse ScrollWheel" );
+            transform.localScale = new Vector3( x, y, 1 );
+        }
     }
 
-    //Use scroll to scale
-    //use mouse click to assign and move 4 corners.
-
     public void OnBeginDrag( PointerEventData eventData ) {
-        //distance = Camera.main.ScreenToWorldPoint( new Vector3( Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint( transform.position ).z ) ) - transform.position;
-        //dist = Vector2.Distance( transform.position, Input.mousePosition );
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-        worldPos.z = transform.position.z;
-        offset = worldPos - transform.position;
+        if( panZoomImage ) {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+            worldPos.z = transform.position.z;
+            offset = worldPos - transform.position;
+        }
     }
 
     public void OnDrag( PointerEventData eventData ) {
-        //transform.position = new Vector3( Input.mousePosition.x - dist, Input.mousePosition.y - dist, transform.position.z );
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-        worldPos.z = transform.position.z;
-        worldPos = worldPos - offset;
-        transform.position = worldPos;
+        if( panZoomImage ) {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+            worldPos.z = transform.position.z;
+            worldPos = worldPos - offset;
+            transform.position = worldPos;
+        }
     }
 
     public void OnEndDrag( PointerEventData eventData ) {
-        offset = Vector3.zero;
+        if( panZoomImage ) {
+            offset = Vector3.zero;
+        }
+    }
+
+    public void togglePanZoom() {
+        panZoomImage = !panZoomImage;
     }
 }
