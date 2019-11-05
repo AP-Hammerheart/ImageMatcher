b284643d73b39bd5fbaba7d519a0b32f6dbd26c0
Adrian <adrian.parneus@gmail.com>
2019-11-05 18:09:30 +0100

    read ndpi file
    Change: reads filepath to ndpi instead since full ndpi is connected to part of macro image.

--- Assets/Pathology/Code/PathologyMicroHandler.cs
+++ Assets/Pathology/Code/PathologyMicroHandler.cs
@@ -4,55 +4,32 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
-using OpenSlide;
+
 
 public class PathologyMicroHandler : MonoBehaviour {
+    
+    private string filePath = "";
 
-    public string defaultPath = "D:\\Other Projects\\Hololens4Pathology and Radiology\\Pancreas_T2747-19\\pathology\\2D";
-    public Image preview;
-    public List<Texture2D> images = new List<Texture2D>();
+    public string defaultPath = "D:\\Other Projects\\Hololens4Pathology and Radiology\\Pancreas_T2747-19\\histology";
+    public Text preview;
 
     public void LoadPathologyMicroButton() {
-        string path = EditorUtility.OpenFilePanel( "Select Pathology Micro..", defaultPath, "" );
-        if( path.Length != 0 ) {
-            //byte[] data;
-            //data = File.ReadAllBytes( path );
-            //image = new Texture2D( 2, 2 );
-            //image.LoadImage( data );
-            //Sprite s = Sprite.Create( image, new Rect( 0, 0, image.width, image.height ), Vector2.zero );
-            //preview.sprite = s;
-            //preview.SetNativeSize();
+        filePath = EditorUtility.OpenFilePanel( "Select Pathology Micro..", defaultPath, "" );
 
-            const int CHUNK_SIZE = 1024;
+        filePath = filePath.Replace( "/", "\n" );
 
-            using( FileStream stream = new FileStream( path, FileMode.Open, FileAccess.Read ) ) {
-                using( BinaryReader reader = new BinaryReader( stream, new System.Text.ASCIIEncoding() ) ) {
-                    byte[] chunk;
+        preview.text = filePath;
+        /*
+         * 1. hämta sökväg till ndpi
+         * 2. connecto ndpi sökväg till markerade område i macro
+         * 3. som sedan konnectas till en bild i en serie av radiologi.
+         * 
+         * 
+         * [R] <- R index MA rect -> [MA] <- MA rect -> [MI]
+         * 
+         * 
+        */
 
-                    chunk = reader.ReadBytes( CHUNK_SIZE );
-                    //while( chunk.Length > 0 ) {
-                        DumpBytes( chunk, chunk.Length );
-                    //    chunk = reader.ReadBytes( CHUNK_SIZE );
-                    //}
-                }
-            }
-
-        }
-    }
-
-    private void DumpBytes(byte[] chunk, int length) {
-
-        Texture2D image = null;
-        image = new Texture2D( 2, 2 );
-        image.LoadImage( chunk );
-        images.Add( image );
-
-
-        Material mat = new Material(Shader.Find("Standard"));
-        mat.SetTexture( "Albedo", image );
-        GameObject g = GameObject.CreatePrimitive( PrimitiveType.Quad );
-        g.transform.SetParent( transform );
-        g.GetComponent<Renderer>().material = mat;
 
 
     }
