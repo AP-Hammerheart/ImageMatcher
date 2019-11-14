188c13ccf130db50d14377b56d8e2606299233ee
Adrian <adrian.parneus@gmail.com>
2019-11-14 03:25:36 +0100

    New UI Progress
    DICOM
    MACRO
    MICRO
    
    TODO:
    TOOLS
    CONNECT
    TEST

---
+++ Assets/Tools/Code/ToolMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ToolModes {
+    PanZoom,
+    MarkArea,
+}
+
+public class ToolMode : MonoBehaviour {
+    public ToolModes toolMode = ToolModes.PanZoom;
+}
---
+++ Assets/UI/MenuBar/Code/LoadMicroButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class LoadMicroButton : MonoBehaviour {
+
+    [Header( "Default Variables" )]
+    public string defaultPath = "D:\\Other Projects\\Hololens4Pathology and Radiology\\Pancreas_T2747-19\\histology";
+
+    [Header( "UI" )]
+    public Transform WorkArea;
+
+    [Header( "Data Holder" )]
+    public Transform PathologyMicroHandler;
+
+    [Header( "Prefabs" )]
+    public PreviewImageWindow previewWindowPrefab;
+    public PathologyMicroData pathologyMicroDataPrefab;
+
+    public void LoadPathologyMicroButton() {
+        string filePath = EditorUtility.OpenFilePanel( "Select Pathology Micro..", defaultPath, "" );
+        if( !filePath.Contains( ".ndpi" ) ) {
+            filePath = "";
+        } else {
+            PathologyMicroData pmd = Instantiate( pathologyMicroDataPrefab, Vector3.zero, Quaternion.identity, PathologyMicroHandler ) as PathologyMicroData;
+            pmd.imagePath = filePath;
+
+            PreviewImageWindow piw = Instantiate( previewWindowPrefab, WorkArea, false );
+            piw.GetComponent<WindowBar>().windowName.text = filePath;
+        }
+    }
+}
